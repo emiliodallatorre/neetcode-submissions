8b4d53b62ea9f4809f4a6be663dd8bce3feacbba
Emilio Dalla Torre <info@emiliodallatorre.it>
2026-08-13 16:16:46 +0100

    Add: kth-largest-integer-in-a-stream - submission-9

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-9.py
@@ -0,0 +1,35 @@
+class KthLargest:
+    content: List[int]
+    k: int
+
+    def __init__(self, k: int, nums: List[int]):
+        self.content = []
+        self.k = k
+
+        for n in nums:
+            self.add(n)
+
+    def add(self, val: int) -> int:
+        if not self.content:
+            self.content.append(val)
+            return val
+        
+        pos: int = len(self.content)
+        par: int = (pos - 1) // 2
+        self.content.append(val)
+
+        while self.content[pos] <= self.content[par] and pos > 0:
+            self.content[par], self.content[pos] = self.content[pos], self.content[par]
+            pos, par = par, (par - 1) // 2
+
+        if len(self.content) > self.k:
+            self.content.pop(0)
+            content: list = [x for x in self.content]
+            self.content = []
+
+            for n in content:
+                self.add(n)
+
+        result: int = self.content[0]
+        return result
+        
